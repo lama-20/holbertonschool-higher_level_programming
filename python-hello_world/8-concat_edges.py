51c749ee3fd8a29468c4ac7e88100d3f3bdb111c
lama <lama72316@gmail.com>
2026-06-06 14:40:35 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-new_word = word[3:] + "-object-oriented programming" + word[:2] + word[5:]
-print(new_word)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[:6]
+print(str)
