91595812315eedb6df7413e35813aaeafd4f799d
lama <lama72316@gmail.com>
2026-06-13 18:09:16 +0000

    Fix NaN handling

---
+++ 0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for adding two integers."""
+
+
+def add_integer(a, b=98):
+    """Add two integers and return the result."""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if a != a:
+        raise ValueError("cannot convert float NaN to integer")
+
+    if b != b:
+        raise ValueError("cannot convert float NaN to integer")
+
+    return int(a) + int(b)
