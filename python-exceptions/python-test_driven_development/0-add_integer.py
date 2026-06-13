5a18a3ea11ceba7a9c6cb15a38b1d2cb8a135b2f
lama <lama72316@gmail.com>
2026-06-13 18:03:41 +0000

    Add 0-add_integer

---
+++ python-exceptions/python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for adding two integers."""
+
+
+def add_integer(a, b=98):
+    """Add two integers.
+
+    Args:
+        a: First number.
+        b: Second number.
+
+    Returns:
+        The integer sum of a and b.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
