a9695a2aa72745a7c571f25ea5775701b393b9bc
lama-20 <lama72316@gmail.com>
2026-06-27 18:14:19 +0000

    Add Dragon mixins

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module that defines mixins and Dragon."""
+
+
+class SwimMixin:
+    """Mixin that adds swimming ability."""
+
+    def swim(self):
+        """Print swimming message."""
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin that adds flying ability."""
+
+    def fly(self):
+        """Print flying message."""
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon class."""
+
+    def roar(self):
+        """Print roaring message."""
+        print("The dragon roars!")
