1689308e05b47bd1981196fee877f95e96c51a20
lama-20 <lama72316@gmail.com>
2026-06-27 18:00:09 +0000

    Add Square class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines Square."""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize Square."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the square area."""
+        return self.__size * self.__size
