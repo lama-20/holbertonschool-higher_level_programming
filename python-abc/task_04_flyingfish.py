34690347ae838d20a2c659c04acfd86aae0e3b7f
lama-20 <lama72316@gmail.com>
2026-06-27 18:12:53 +0000

    Add FlyingFish multiple inheritance

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Module that defines Fish, Bird, and FlyingFish."""
+
+
+class Fish:
+    """Fish class."""
+
+    def swim(self):
+        """Print swimming message."""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Print habitat message."""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Bird class."""
+
+    def fly(self):
+        """Print flying message."""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Print habitat message."""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """FlyingFish class."""
+
+    def swim(self):
+        """Print swimming message."""
+        print("The flying fish is swimming!")
+
+    def fly(self):
+        """Print flying message."""
+        print("The flying fish is soaring!")
+
+    def habitat(self):
+        """Print habitat message."""
+        print("The flying fish lives both in water and the sky!")
