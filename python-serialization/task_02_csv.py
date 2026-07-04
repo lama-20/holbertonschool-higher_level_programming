9a7d81270896a02ff7c798aee771e19c0421e0a7
lama-20 <lama72316@gmail.com>
2026-07-04 18:29:31 +0000

    Add CSV to JSON converter

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module to convert CSV data to JSON."""
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """Convert CSV data to JSON.
+
+    Returns:
+        True if the conversion succeeds, otherwise False.
+    """
+    try:
+        with open(filename, "r", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with open("data.json", "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+
+    except FileNotFoundError:
+        return False
