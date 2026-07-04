4b590f538c615e7a5c5ae9db27b82a5b1560bd99
lama-20 <lama72316@gmail.com>
2026-07-04 18:23:50 +0000

    Add Student reload from JSON

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that defines Student."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the instance."""
+        if type(attrs) is list:
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
