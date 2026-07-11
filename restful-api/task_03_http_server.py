234b5337b31913c1abfa5459d7bd0e51222f8e97
lama <lama72316@gmail.com>
2026-07-11 21:31:54 +0300

    Fix 404 response for HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,11 +39,10 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode("utf-8"))
+    self.send_response(404)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
